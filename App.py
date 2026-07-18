85da50637168436db91e80b8d6abb41dece187da
diegoborges <diegodavilaborges20242@gmail.com>
2026-07-18 17:06:33 -0300

    Ajustando Títulos 4

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ st.subheader("🧰 Assistência Técnica ")
 
 st.markdown("### 🧰 Assistência Técnica") # O número de hashtags controla o tamanho
 # Ou, para ter controle total da fonte (ex: 24 pixels):
-st.markdown("<h2 style='text-align: left; font-size: 24px;'>🧰 Assistência Técnica</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: left; font-size: 32px;'>🧰 Assistência Técnica</h2>", unsafe_allow_html=True)
 
 # Criamos botões de rádio para escolher a operação
 opcao = st.radio("O que você deseja calcular?", 
