45ea1a62a8d95a6cc2daf3cb0f27b9d6191eb70c
diegoborges <diegodavilaborges20242@gmail.com>
2026-07-18 16:57:52 -0300

    Cálculos de dimensionamento de motor

--- App.py
+++ App.py
@@ -5,7 +5,39 @@ st.title("🧰 Assistência Técnica ")
 
 # Criamos botões de rádio para escolher a operação
 opcao = st.radio("O que você deseja calcular?", 
-                 ["Força de Avanço do Cilindro", "Força de Recuo do Cilindro", "Velocidade de Avanço do Cilindro"], index=None)
+                 ["Força de Avanço do Cilindro", "Força de Recuo do Cilindro", "Velocidade de Avanço do Cilindro", "Cálculos de Motor Hidráulico"], index=None)
+
+
+
+if opcao == "Cálculos de Motor Hidráulico":
+    st.subheader("Cálculos de Motor Hidráulico")
+    
+    # Slider para eficiência (isso facilita muito para o técnico)
+    eficiencia = st.slider("Eficiência do motor (%)", 0, 100, 85)
+    fator = 456 * (eficiencia / 100)
+    
+    sub_opcao = st.radio("O que deseja calcular?", ["Vazão máxima", "Pressão máxima", "Potência mínima"],index=None )
+    
+    if sub_opcao == "Vazão máxima":
+        potencia = st.number_input("Potência (CV)", min_value=0.0)
+        pressao = st.number_input("Pressão (bar)", min_value=0.0)
+        if st.button("Calcular"):
+            vazao = (fator * potencia) / pressao if pressao != 0 else 0
+            st.write(f"Vazão máxima: {vazao:.2f} L/min")
+            
+    elif sub_opcao == "Pressão máxima":
+        potencia = st.number_input("Potência (CV)", min_value=0.0)
+        vazao = st.number_input("Vazão (L/min)", min_value=0.0)
+        if st.button("Calcular"):
+            pressao = (fator * potencia) / vazao if vazao != 0 else 0
+            st.write(f"Pressão máxima: {pressao:.2f} bar")
+            
+    elif sub_opcao == "Potência mínima":
+        vazao = st.number_input("Vazão (L/min)", min_value=0.0)
+        pressao = st.number_input("Pressão (bar)", min_value=0.0)
+        if st.button("Calcular"):
+            potencia = (vazao * pressao) / fator
+            st.write(f"Potência mínima: {potencia:.2f} CV")
 
 # A lógica abaixo só aparece se a opção for selecionada
 if opcao == "Força de Avanço do Cilindro":
